e5c1feba73844303d93dbc9876232bd2218eb505
vlad <mlprimitif@gmail.com>
2024-08-01 17:41:31 +0700

    AUTH basic admin and user

--- app/database/migrations/2014_10_12_000000_create_users_table.php
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,21 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string("avatar")->default("/uploads/default_avatar.png");
             $table->string('email')->unique();
+            $table->enum("role", ['user','admin'])->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
     }
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
 
     /**
      * Reverse the migrations.
